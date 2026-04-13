d32f571f52ab41b73338bb8814f1df50fc658ca6
DanielMachluf <danielmachluf420@gmail.com>
2026-04-13 16:32:56 +0300

    stayed with the deafault colors

--- Frontend/src/Components/PagesArea/Home/Home.tsx
+++ Frontend/src/Components/PagesArea/Home/Home.tsx
@@ -4,8 +4,7 @@ export function Home() {
 
     return (
         <div className="Home">
-            <h1>Explore the world with <span>Vacational</span></h1>
-            <p>Discover handpicked vacation destinations, track your favorites, and plan your next adventure — all in one place.</p>
+            <p>Home Component</p>
         </div>
     );
 }
